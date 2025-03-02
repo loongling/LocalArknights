06fd7409bef9ee2185600bc1ef1e7024c145b2a2
LoongLing <110767767+loongling@users.noreply.github.com>
2025-03-02 13:54:44 +0800

    v0.0.6
    新增新版本新接口GET /general/v1/server_time

---
+++ src/main/java/com/hypergryph/arknights/general/v1.java
@@ -0,0 +1,47 @@
+package com.hypergryph.arknights.general;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
+
+@RequestMapping("/general/v1")
+public class v1 {
+
+    private static final String HOLIDAY_API_URL = "https://timor.tech/api/holiday/info/";
+
+    @GetMapping("/server_time")
+    public JSONObject serverTime() {
+        long UnixTime = System.currentTimeMillis();
+        LocalDate today = LocalDate.now();
+        boolean isHoliday = checkIfHoliday(today.toString());
+
+        JSONObject data  = new JSONObject();
+        data.put("isHoliday", isHoliday);
+        data.put("UnixTime", UnixTime);
+
+        JSONObject result = new JSONObject();
+        result.put("data", data);
+        result.put("msg", "OK");
+        result.put("status", 0);
+
+        return result;
+    }
+
+    private boolean checkIfHoliday(String date) {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            String url = HOLIDAY_API_URL + date;
+            JSONObject response = restTemplate.getForObject(url, JSONObject.class);
+            if (response != null && response.getBoolean("holiday")) {
+                return true;
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
